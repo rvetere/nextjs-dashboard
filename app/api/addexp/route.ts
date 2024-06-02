668f0155d256e33f020ae5738765615880303d15
Remo Vetere <remo.vetere@gmail.com>
2024-06-02 15:43:39 +0200

    Fixes

--- app/api/addexp/route.ts
+++ app/api/addexp/route.ts
@@ -3,15 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { unstable_noStore as noStore } from 'next/cache';
 import { ExpensesType } from '@/app/lib/definitions';
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
+  noStore();
+  const dataStr = req.nextUrl.searchParams.get('d');
+  const received = JSON.parse(dataStr || '{}');
   const { total_expenses, carried_over, daily_budget, settings_budget } =
-    JSON.parse(req.headers.get('data') || '{}');
-  console.log('GET request /api/addexp', {
-    total_expenses,
-    carried_over,
-    daily_budget,
-    settings_budget,
-  });
+    received;
   const date = new Date().toISOString().split('T')[0];
   const data = await sql<ExpensesType>`
         INSERT INTO expenses (total_expenses, carried_over, daily_budget, settings_budget, created_at)
