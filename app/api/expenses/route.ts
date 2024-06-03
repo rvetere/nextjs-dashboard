f498b2fa06c5f535fa753e6a1f2b2977a073d8da
Remo Vetere <remo.vetere@gmail.com>
2024-06-03 17:36:10 +0200

    Add ip to expense

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -1,16 +1,21 @@
 import { sql } from '@vercel/postgres';
 import { NextRequest, NextResponse } from 'next/server';
 import { unstable_noStore as noStore } from 'next/cache';
-import { ExpensesType } from '@/app/lib/definitions';
+import { ExpensesType, IpType } from '@/app/lib/definitions';
 
 async function fetchExpenses() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
   try {
+    const dataIp =
+      await sql<IpType>`SELECT * FROM current_ip ORDER BY id DESC LIMIT 1`;
     const data =
       await sql<ExpensesType>`SELECT * FROM expenses ORDER BY id DESC LIMIT 1`;
-    return data.rows;
+    return data.rows.map((row, index) => ({
+      ...row,
+      ip: dataIp.rows[index]?.ip,
+    }));
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch expenses data.');
